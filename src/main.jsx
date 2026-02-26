82f72e67620cffd303bb9231f542f28426bb274f
anaszone <mohammedanaschowdhury@gmail.com>
2026-02-26 16:40:26 +0600

    create components and layouts

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,29 @@ import App from "./App.jsx";
 
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import MainLayout from "./layouts/MainLayout.jsx";
+import Home from "./components/Home.jsx";
+import AddCoffee from "./components/AddCoffee.jsx";
+import UpdateCoffee from "./components/UpdateCoffee.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "addcoffee",
+        Component: AddCoffee,
+      },
+      {
+        path: "updatecoffee",
+        Component: UpdateCoffee,
+      },
+    ],
   },
 ]);
 
